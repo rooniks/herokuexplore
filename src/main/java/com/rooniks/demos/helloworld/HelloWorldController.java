e454b10e71c1bc7a06c879c5f63d576e49e1f7aa
Rooniks <me.rooniks@gmail.com>
2020-07-06 03:53:20 +0530

    Dummy api to test out how config parameters work in Heroku

--- src/main/java/com/rooniks/demos/helloworld/HelloWorldController.java
+++ src/main/java/com/rooniks/demos/helloworld/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.rooniks.demos.helloworld;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +9,22 @@ import java.util.Map;
 
 @RestController
 public class HelloWorldController {
+
+    @Value("${dummyValue}")
+    private String dummyValue;
+
     @GetMapping(value = "/api/v1/hello")
     public Map<String, String> sayHello() {
         Map<String, String> hello = new HashMap<>();
         hello.put("message", "Hello world! What's up?");
         return hello;
+    }
+
+    // Dummy api to test out how config parameters work in Heroku
+    @GetMapping(value = "api/v1/configvalue")
+    public Map<String, String> getConfigValue() {
+        Map<String, String> config = new HashMap<>();
+        config.put("value", dummyValue);
+        return config;
     }
 }
